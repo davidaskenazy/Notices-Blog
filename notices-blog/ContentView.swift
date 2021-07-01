f2c2474f435b3248480a7ba83737c7b35fcd5f56
David Askenazy <davidaskenazy@Air-de-David.fibertel.com.ar>
2021-07-01 16:01:19 -0300

    inicio de proyecto

---
+++ notices-blog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notices-blog
+//
+//  Created by David Askenazy on 01/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notices-blog/notices_blogApp.swift
@@ -0,0 +1,17 @@
+//
+//  notices_blogApp.swift
+//  notices-blog
+//
+//  Created by David Askenazy on 01/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct notices_blogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
